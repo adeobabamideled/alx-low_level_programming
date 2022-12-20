ea3ee85b0a91d667a0aa0663fbffbd912c9ffa50
adeobabamideled <adeobabamideled@gmail.com>
2022-12-20 19:47:20 +0100

    dd

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,43 @@
-#include "main.h"
+#include "main.h"                                                                                                                    
 
-/**
+                                                                                                                                     
 
- * reset_to_98 - a function that takes a pointer to an int as parameter
+/**                                                                                                                                  
 
- * and updates the value it points to to 98
+ * strcat - Concatenates the string pointed to by @src, including the x                                                    
 
- * @n: input
+ *          null byte, to the end of the string pointed to by @dest.                                                                
 
- * Return: n
+ * @dest: A pointer to the string to be concatenated upon.                                                                          
 
- */
+ * @src: The source string to be appended to @dest.                                                                                  
 
-void reset_to_98(int *n)
+ *                                                                                                                                  
 
-{
+ * Return: A pointer to the destination string @dest.                                                                                
 
-        *n = 98;
+ */                                                                                                                                  
+
+char *_strcat(char *dest, const char *src)                                                                                            
+
+{                                                                                                                                    
+
+        int index = 0, dest_len = 0;                                                                                                
+
+                                                                                                                                     
+
+        while (dest[index++])                                                                                                        
+
+                dest_len++;                                                                                                          
+
+                                                                                                                                     
+
+        for (index = 0; src[index]; index++)                                                                                        
+
+                dest[dest_len++] = src[index];                                                                                      
+
+                                                                                                                                     
+
+        return (dest);                                                                                                              
 
 }
-
-
